524d8b16308f6f16d3a58b29e2922d9a115d87b8
enrico.mosca <enrico.mosca@valtech.com>
2021-06-08 14:59:31 +0200

    removed unused function

--- module/excelFileModule.py
+++ module/excelFileModule.py
@@ -23,44 +23,6 @@ def aggregate_csv_files(file_name, all_filenames):
     print(Style.RESET_ALL)
 
 
-def merge_excel_files2(main_file, file_to_merge):
-    if main_file and file_to_merge:
-        # xl_main_sheets = pd.ExcelFile(main_file).sheet_names
-        # xl_to_merge_sheets = pd.ExcelFile(file_to_merge).sheet_names
-        # # open the excel writer in append mode
-        # appender = pd.ExcelWriter(main_file, mode='a')
-        # for s_main in xl_main_sheets:
-        #     for s_to_merge in xl_to_merge_sheets:
-        #         if s_main == s_to_merge:
-        #             print(s_main)
-
-        # appender.save()
-        # appender.close()
-        # load all the sheets from an excel file inside a dataframe {sheet_name:dataframe}
-        #  for k in df_to_merge.keys():
-        # for s in df_to_merge[k]:
-        #     for d in df_to_merge[k][s]:
-        #         # if df_to_merge[k][s].startswith('http'):
-        #         if isinstance(d, str) and d.startswith('http'):
-        #             print('bah')
-        #             print(d)
-        frames = {}
-        df_main = pd.read_excel(main_file, index_col=0, sheet_name=None)
-        df_to_merge = pd.read_excel(
-            file_to_merge, index_col=0, sheet_name=None,)
-        for k in df_to_merge.keys():
-            for km in df_main.keys():
-                if k == km:
-                    frames[k] = df_main[k].append(df_to_merge[k])
-        appender = pd.ExcelWriter(main_file, mode='a', engine="openpyxl")
-        for k in frames.keys():
-            print(k)
-            df = pd.DataFrame(data=frames[k])
-            print(df)
-            df.to_excel(appender, sheet_name=k, index=False)
-        appender.save()
-        appender.close()
-
 # based on https://gist.github.com/Trambadiya26Nensi/a31d186f1aba549b3fef0f7b55945e20#file-appenddata-py
 
 
